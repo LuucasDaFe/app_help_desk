39dc4d6598c88407bd6d9d4070d1ccd6a9049503
Lucas Abraao <Labraao957@gmail.com>
2023-06-17 21:33:54 -0300

    Autenticando o usuário

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,34 @@
 |<?php 
-    
+    //variavel que verifica se a autenticacao foi realizada.
+    $usuario_autenticado = false;
+
+    //Usuarios do sistema 
+    $usuario_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+        array('email' => 'labraao957@gmail.com', 'senha' => 'cr7melhor'),
+    );
+
+    /*
+    echo '<pre>';
+    print_r($usuario_app);
+    echo '</pre>';
+    */
+
+    foreach($usuario_app as $user){
+        
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+        }
+        
+    };
+
+    if($usuario_autenticado){
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
     /*
     print_r($_GET);
 
@@ -10,10 +39,9 @@
     echo $_GET['senha'];
     */
 
+    /*
     print_r($_POST);
     echo '<br />';
-
-    echo $_POST['email'];
-    echo '<br />';
-    echo $_POST['senha'];
+    */
+   
 ?>
