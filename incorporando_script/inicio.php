c977a98e28f66f0e6f6debd4fb54fb9e6d386e48
Lucas Abraao <Labraao957@gmail.com>
2023-06-17 23:03:37 -0300

    Incorporando scripts com include

---
+++ incorporando_script/inicio.php
@@ -0,0 +1,16 @@
+<?php 
+    include_once "menu.php"; // warning
+
+    //require ("menu2.php") // fatal error
+
+?>
+
+conteúdo da página (início)
+<br />
+
+<?php 
+    include_once "menu.php"; // warning
+
+    //require ("menu2.php") // fatal error
+
+?>
